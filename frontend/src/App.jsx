d58f176bcb361750c92a2e29411c7dcadbc35491
Amit Kumar Mandal <amitkr123069@gmail.com>
2025-06-19 19:21:00 +0530

    added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import { useState } from 'react'
-
+import React from 'react'
 import './App.css'
-
-
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+function App(){
+  <>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+    </Routes>
+  </>
+}
 
 export default App
